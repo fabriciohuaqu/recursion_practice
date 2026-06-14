9699923923bfdf79959f5e687b35fd55b18dc4a6
fabriciohuaqu <fhuayhuaqu@unsa.edu.pe>
2026-06-14 11:59:20 -0500

    +pract

--- recursion_1/count8.c
+++ recursion_1/count8.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
-
-int main()
-{
-	return 0;
+/*Given a non-negative int n, compute recursively (no loops) the count of the occurrences of 8 as a digit, 
+except that an 8 with another 8 immediately to its left counts double, so 8818 yields 4. */
+int count8(int n){
+	if(n < 10)
+		return (n == 8);
+	if(n % 10 == 8 && (n / 10) % 10 == 8)
+		return 2 + count8(n / 10);
+	return (n % 10 == 8) + count8(n / 10);
 }
-
